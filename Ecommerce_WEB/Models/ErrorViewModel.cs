c3cddc6e2814993b5061bfeb1ed3d7c68f0ba50a
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-06-15 08:19:34 -0300

    Creacion de proyecto

---
+++ Ecommerce_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
